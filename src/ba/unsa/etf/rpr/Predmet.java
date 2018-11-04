602e4692a851f011c905499ed26caf48440b256a
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-04 22:15:09 +0100

    Klasa Predmet

---
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+
+public class Predmet {
+    private String ime;
+    private Integer opterecenje;
+    private boolean izborni;
+    private ArrayList<Student> spisak;
+    private Semestar semestar;
+
+    public Predmet(String ime, Integer opterecenje, boolean izborni) {
+        this.ime = ime;
+        this.opterecenje = opterecenje;
+        this.izborni = izborni;
+    }
+
+    public String dajIme() {
+        return ime;
+    }
+
+    public Integer dajOpterecenje() {
+        return opterecenje;
+    }
+
+    public boolean jeLiIzborni() {
+        return izborni;
+    }
+
+    public ArrayList<Student> dajSpisak() {
+        return spisak;
+    }
+
+    public Semestar dajSemestar() {
+        return semestar;
+    }
+
+    public void upisi(Student student){
+
+    }
+
+    public void ispisi(Student student){
+
+    }
+}
