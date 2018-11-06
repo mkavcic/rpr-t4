fbf305061779a151a7d077bcf5aeada81d48dca3
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-06 17:52:23 +0100

    Prepravljene klase po novom dijagramu - dodane klase PlanStudija, Upis i Fakultet

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -1,13 +1,9 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
-
 public class Predmet {
     private String ime;
     private Integer opterecenje;
     private boolean izborni;
-    private ArrayList<Student> spisak;
-    private Integer semestar;
 
     public Predmet(String ime, Integer opterecenje, boolean izborni) {
         this.ime = ime;
@@ -25,17 +21,5 @@ public class Predmet {
 
     public boolean jeLiIzborni() {
         return izborni;
-    }
-
-    public ArrayList<Student> dajSpisak() {
-        return spisak;
-    }
-
-    public void upisi(Student student){
-
-    }
-
-    public void ispisi(Student student){
-
     }
 }
--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -1,24 +1,14 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
-
 public class Student {
     private String ime;
     private String prezime;
     private Integer brojIndexa;
-    private Integer brojEcts;
-    private Integer semestar;
-    private final Integer brojObaveznihEcts=30;
-    private Integer brojIzbornihNaSemestru;
-    private Integer brojObaveznihNaSemestru;
-    private final Integer brojIzbornih=0;
-    private final Integer brojObaveznih=0;
 
-    public Student(String ime, String prezime, Integer brojIndexa, Integer brojEcts, Integer semestar) {
+    public Student(String ime, String prezime, Integer brojIndexa) {
         this.ime = ime;
         this.prezime = prezime;
         this.brojIndexa = brojIndexa;
-        this.semestar = semestar;
     }
 
     public String dajIme() {
@@ -32,27 +22,5 @@ public class Student {
     public Integer dajBrojIndexa() {
         return brojIndexa;
     }
-
-    public Integer dajBrojEcts() {
-        return brojEcts;
-    }
-
-    public Integer dajBrojIzbornihNaSemestru() {
-        return brojIzbornihNaSemestru;
-    }
-
-    public Integer dajBrojObaveznihhNaSemestru() {
-        return brojObaveznihNaSemestru;
-    }
-
-    public Integer dajBrojObaveznih() {
-        return brojObaveznih;
-    }
-
-    public Integer dajBrojIzbornih() { return brojIzbornih; }
-
-    public void postavibrojIzbornihNaSemestru(Integer broj) { this.brojIzbornihNaSemestru=broj; }
-
-    public void postavibrojObaveznihNaSemestru(Integer broj) { this.brojObaveznihNaSemestru=broj;  }
 
 }
