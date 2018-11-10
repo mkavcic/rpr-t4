8c76a8494f15b2516033e8baead85d7e02ea24fd
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-10 17:21:02 +0100

    Fakultet - prepravka u setovima

--- src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
@@ -1,11 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal04;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Fakultet {
-    private Set<Student> studenti;
-    private Set<Predmet> predmeti;
-    private Set<Upis> upisani;
+    private Set<Student> studenti = new HashSet<>();
+    private Set<Predmet> predmeti = new HashSet<>();
+    private Set<Upis> upisani = new HashSet<>();
 
 
     public void dodajStudenta(Student s){
@@ -21,5 +22,6 @@ public class Fakultet {
     }
 
     public void upisi(Student student, PlanStudija studij,  Predmet predmet){ upisani.add( new Upis(student, studij, predmet)); }
+
 }
 
