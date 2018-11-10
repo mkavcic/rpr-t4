7a5348f7e8272cc7ae8f61da82403accd27ce9b7
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-10 16:19:16 +0100

    Main - novi studenti

--- src/ba/unsa/etf/rpr/tutorijal04/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Main.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr.tutorijal04;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Student s1 = new Student("Mirna", "Kavčić", 17925);
+	Student s2 = new Student("Hana", "Hanić", 17926);
+	Student s3 = new Student("Nedim", "Osmić", 17927);
+	Student s4 = new Student("Meho", "Mehić", 17928);
+	Student s5 = new Student("Dino", "Sikira", 17929);
+	Student s6 = new Student("Anela", "Sikira", 17930);
+
+
     }
 }
