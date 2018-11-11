9311140d4064c4d8dc0771aac43415f0aef1ab9f
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-11 14:19:55 +0100

    Upis - konstruktor pomaknut iz atributa

--- src/ba/unsa/etf/rpr/tutorijal04/Upis.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Upis.java
@@ -2,15 +2,14 @@ package ba.unsa.etf.rpr.tutorijal04;
 
 public class Upis {
     private Student student;
+    private PlanStudija studij;
+    private Predmet predmet;
 
     public Upis(Student student, PlanStudija studij, Predmet predmet) {
         this.student = student;
         this.studij = studij;
         this.predmet = predmet;
     }
-
-    private PlanStudija studij;
-    private Predmet predmet;
 
     public Student getStudent() {
         return student;
