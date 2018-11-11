d9031d8e59c15ffb997db26cc8c7e24b39122457
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-11 14:04:02 +0100

    Fakultet - dodan test za upis i spisak - reformatiran kod

--- Test/ba/unsa/etf/rpr/tutorijal04/FakultetTest.java
+++ Test/ba/unsa/etf/rpr/tutorijal04/FakultetTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FakultetTest {
     @Test
-    void fakultetTest(){
+    void fakultetTest() {
         Fakultet fakultet = new Fakultet();
-        Student s1= new Student("Mirna", "Kavčić", 17925);
-        Student s2= new Student("Meho", "Mehić", 17926);
-        Predmet p1=new Predmet("Razvoj programskih rješenja", 5, true);
-        Predmet p2= new Predmet("Inžejerkska matematika 2", 6, false);
-        PlanStudija bolonja= new PlanStudija();
+        Student s1 = new Student("Mirna", "Kavčić", 17925);
+        Student s2 = new Student("Meho", "Mehić", 17926);
+        Predmet p1 = new Predmet("Razvoj programskih rješenja", 5, true);
+        Predmet p2 = new Predmet("Inžejerkska matematika 2", 6, false);
+        PlanStudija bolonja = new PlanStudija();
         bolonja.dodajPredmet(3, p1);
         bolonja.dodajPredmet(2, p2);
         fakultet.dodajStudenta(s1);
@@ -21,8 +21,8 @@ class FakultetTest {
         fakultet.dodajPredmet(p2);
         fakultet.upisi(s1, bolonja, p1);
         fakultet.upisi(s2, bolonja, p1);
-        fakultet.upisi(s1, bolonja,p2);
-        fakultet.upisi(s2,bolonja,p2);
+        fakultet.upisi(s1, bolonja, p2);
+        fakultet.upisi(s2, bolonja, p2);
         assertEquals("Meho Mehić\nMirna Kavčić\n", fakultet.dajSpisak(p1));
     }
 
